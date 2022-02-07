809a08982af426a156e7613d36879af25f41e9e3
firstChairCoder <md1qq7@gmail.com>
2022-02-07 02:08:18 +0100

    Added StatusBar to aid in scheme identification

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { func } from "./constants";
 
 import RootStack from "./src/navigation/Stack";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <AppLoading
+        onError={() => console.warn("An error occured on loading!")}
+        onFinish={() => setLoading(false)}
+        startAsync={func.loadAssetsAsync}
+      />
+    );
+  }
+
   return (
     <>
+      <StatusBar style="auto" />
       <RootStack />
     </>
   );
